336af594fe508e4e9589eef772b1c63b89f29b0c
tcarrere <tomascarrere4321@gmail.com>
2022-11-27 22:01:53 -0300

    tarea 11.3

---
+++ javascript/app.js
@@ -0,0 +1,5 @@
+let apariciones = parseInt(prompt("¿Cuantas veces quieres que aparezca la frase HOLA MUNDO?"));
+    for(i=0;i<apariciones;i++)
+    {
+             document.write("Hola mundo!" +" "+ i + "<br>");
+    }
